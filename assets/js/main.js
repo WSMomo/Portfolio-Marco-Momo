52e08896291d51618f13a5df911bb6c11649fd28
Momo M <momomarcoj@gmail.com>
2023-03-06 20:13:01 +0100

    chiusura menu dopo click link ok, animazione hamburger-menu ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
+//hamburger menu transform
 function hamburger(x) {
+  if (!x.classList.contains('disabled')) {
+    x.classList.add('disabled');
     x.classList.toggle("change");
+    setTimeout(function() {
+      x.classList.remove('disabled');
+    }, 400);
   }
+}
+
+
+
+
+//collapse navbar after clicking a link
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarLink')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { 
+      bsCollapse.toggle();
+      hamburger(document.querySelector('.hamburger-menu'));
+    });
+});
+
+
+
 
